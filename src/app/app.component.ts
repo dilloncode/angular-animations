60d2f9cfbb8afcef14122ef5f258b831c782f389
Michael Dillon <mdillon@Jerue.com>
2018-02-13 15:54:02 -0500

    states and transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<button [@myTrigger]="state" (click)="toggleState()">My Button</button>`,
+  styles: [],
+  animations: [
+    trigger('myTrigger', [
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+      state('large', style({
+        transform: 'scale(1.4)'
+      })),
+      // transition('small => large', animate('500ms ease-in')),
+      // transition('large => small', animate('500ms ease-out'))
+      //or
+      // transition('small => large, large => small', animate('500ms'))
+      //or
+      transition('small <=> large', animate('500ms'))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  state: string = 'small';
+
+  toggleState() {
+    this.state = this.state === 'small' ? 'large' : 'small';
+  }
 }
